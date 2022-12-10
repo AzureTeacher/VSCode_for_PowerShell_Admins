f5c2b41a63de8f325a7dc3f4db65f9a5ab357fa2
Tom Wechsler <tom@tomwechsler.ch>
2022-12-10 14:16:33 +0100

    edited lines

--- microsoft_cloud_connection.ps1
+++ microsoft_cloud_connection.ps1
@@ -1,3 +1,11 @@
+Get-ExecutionPolicy -List
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
 Install-Module -Name Az -AllowClobber -Verbose -Force
 
 Import-Module -Name Az
+
+Connect-AzAccount
+
+Get-AzSubscription
